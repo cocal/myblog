727f0d7ba28b65af4a7239036e812382d0fca641
cocal <cocaltzq@gmail.com>
2014-06-22 23:42:57 +0800

    note page add

---
+++ msgNote.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+'''
+Created on 2014年6月22日
+
+@author: cocal
+'''
+import tornado.web
+
+class getMsgHandle(tornado.web.RequestHandler):
+    def get(self):
+        self.write('<html><body><form action="/" method="post">'
+                   '<input type="text" name="message">'
+                   '<input type="submit" value="Submit">'
+                   '</form></body></html>')
+
+    def post(self):
+        self.set_header("Content-Type", "text/plain")
+        self.write("You wrote " + self.get_argument("message"))
