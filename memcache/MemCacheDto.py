01932cae9be86f61b417b43a858e176bf40d4588
cocal <cocaltzq@gmail.com>
2014-08-04 23:47:13 +0800

    memcache

---
+++ memcache/MemCacheDto.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+'''
+Created on 2014年8月4日
+
+@author: cocal
+'''
+
+class memCache():
+    ##key
+    key = None
+    val = None
+    lifeCycle = None
+    
---
+++ memcache/memCacheService.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+'''
+Created on 2014年8月4日
+
+@author: cocal
+'''
+from memcache import MemCacheDto
+
+
+__memCache__ = {'version':'0.001Beta'}
+
+def viewAllCache():
+    print __memCache__
+
+def lockItem(key):
+    if __memCache__.has_key(key) :
+        __memCache__[key] = __memCache__.get(key).lock()
+        
+        
+def addCache(key,value,time = None):
+    if __memCache__.has_key(key) :
+        return 'the key is checked'
+    m = MemCacheDto()
+    m.key = key
+    m.val = value
+    m.lifeCycle = time
+    
+    
